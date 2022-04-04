e90b8530b3c6a4fea47d001a72fd0e73b10db1e4
Jordan Halaby <jhalaby@emich.edu>
2022-04-04 16:54:26 -0400

    Start design of messaging system

---
+++ messaging/message_center.php
@@ -0,0 +1,212 @@
+<?php include "../includes/header.php"; ?>
+<?php include "../includes/connect.php"; ?>
+<?php include "search_modal.php"; ?>
+<?php include_once "../helpers/date_conversion.php"; ?>
+<?php include "../helpers/asset_location_modal.php"; ?>
+<?php include "../helpers/location_output.php"; ?>
+
+<section id="messaging_home" class="message_center">
+
+    <h2 class="auctions section_heading"><span>Messaging Center</span></h2>
+
+    <div id="container">
+
+        <div id="message_container">
+
+            <div id="message_container_left">
+                <div id="contact_topper">
+                    <h4 class="contact_heading" id="conversation_heading"><span>Conversations</span></h4>
+
+                    <div id="contact_search_container">
+
+                        <div class="search_bar">
+                            <i class="fa fa-search icon"></i>
+                            <input class="input-field" id="search_conversations" type="text" placeholder="Search Conversations">
+                        </div>
+                    </div>
+                </div>
+                <div id="contact_list">
+
+                    
+                    
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_holder">
+                            <div class="contact_photo">
+                                <div class="image_holder">
+                                    <img src="<?php echo $root; ?>img/no_image.png" alt="No Image">
+                                </div>
+                            </div>
+                            <div class="message_information">
+                                <h5 class="contact_name">Jordan Halaby</h5>
+                                <div class="wrapper">
+                                    <p class="message_content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi perspiciatis ut, ipsum pariatur blanditiis ipsa facilis tempora sequi officiis, quae aliquam maiores est nam perferendis iste illo reprehenderit autem repellendus harum deserunt? Impedit earum aut sequi ad quaerat reprehenderit. Temporibus voluptate veniam harum et ad, nisi aliquid tenetur adipisci corrupti?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div id="seperation_line"></div>
+
+            <div id="message_container_right">
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<script src="../js/jquery.js"></script>
+<script>
+    $(document).ready(function(){
+        var conversation_heading_height = $("#conversation_heading").height() + 40 + 15 - 2;
+        var search_conversations_height = $("#search_conversations").height();
+
+        var height = conversation_heading_height + search_conversations_height;
+    
+        var container_height = $("#container").height();
+
+        var rem = container_height - height;
+
+        $("#contact_list").height(rem);
+        
+    });
+</script>
